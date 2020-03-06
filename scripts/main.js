2ae5c785cdf951ca896674a1de9efd18764c8533
Alex Curnow <alexcurnow@gmail.com>
2020-03-06 15:55:04 -0600

    rendered all components to dom with next to no styling

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,9 @@
 import { RenderEmployeeList } from "./employee/EmployeeList.js";
 import { RenderProductList } from "./product/ProductList.js"
 import { RenderEquipmentList } from "./equipment/EquipmentList.js";
+import { Footer } from "./nav/Footer.js";
+
 RenderEmployeeList()
 RenderProductList()
 RenderEquipmentList()
+Footer()
